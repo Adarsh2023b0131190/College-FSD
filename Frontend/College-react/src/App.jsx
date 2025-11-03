e67d6e9b26fd9bee731923eeeef0a8fd7ececb9f
Adarsh2023b0131190 <adarsh.23b0131190@abes.ac.in>
2025-11-03 12:20:20 +0530

    counter

--- Frontend/College-react/src/App.jsx
+++ Frontend/College-react/src/App.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import Card from './Component/Card';
-import Navbar from './Component/navBar';
+// import React from 'react';
+// import Card from './Component/Card';
+// import Navbar from './Component/navBar';
 
-function App1() {
-  return (
-    <div>
-      <Navbar/>
-    </div>
-  );
-}
 
-function Usestate(){
-  function change(){
-    console.log("Hello");
+// function App1() {
+//   return (
+//     <div>
+//       <Navbar/>
+//     </div>
+//   );
+// }
+
+// function Usestate(){
+//   function change(){
+//     console.log("Hello");
+//   }
+//   return(
+//     <div>
+//       <button onClick={change}>Click</button>
+//     </div>
+//   )
+// }
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(0)
+
+  const addValue = () => {
+    if (counter < 20) setCounter(counter + 1)
   }
-  return(
-    <div>
-      <button onClick={change}>Click</button>
-    </div>
+
+  const removeValue = () => {
+    if (counter > 0) setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>College React</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button><br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
   )
 }
 
-export default App1;
+export default App
---
+++ Frontend/College-react/src/Component menu/counter/CounterApp.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+
+  let [counter,setCounter]=useState(15)
+
+  // let counter=15; 
+
+  const addValue=()=>{
+    // counter+=1;
+    if(counter<20){
+    setCounter(counter+1)
+    }
+  }
+
+  const removeValue=()=>{
+    if(counter>0){
+     setCounter(counter-1)
+    //  console.log("Clicked: ",counter)
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value: {counter}</h2>
+ 
+      <button onClick={addValue}>Add Value</button><br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
